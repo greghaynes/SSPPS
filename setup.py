59e5e5fde650dcd99d055ee3d4bc7705b1923027
Iuri Diniz <iuridiniz@gmail.com>
2012-09-15 17:44:04 -0300

    Update setup.py
    fixing homepage URL

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='sspps',
     description='Super Simple Python Plugin Sytem',
     author='Gregory Haynes',
     author_email='greg@greghaynes.net',
-    url='http://github.com/greghaynes/SSPS',
+    url='http://github.com/greghaynes/SSPPS',
     license='MIT',
     packages=['sspps'],
     )
