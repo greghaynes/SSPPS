eb0b83baddf1dc90c4fafb09e8ca005c3ddef3b7
Gregory Haynes <greg@greghaynes.net>
2012-12-19 22:52:17 -0800

    Bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='sspps',
-    version='0.1.5',
+    version='0.1.6',
     description='Super Simple Python Plugin Sytem',
     author='Gregory Haynes',
     author_email='greg@greghaynes.net',
